038038ec67663e5f7f35876cab81c5ee684d42b1
lorenzo <cmlprinsloo@gmail.com>
2018-06-09 10:45:38 +0200

    added report mailer functions 30m

--- src/main/scala/services/ReportMailer.scala
+++ src/main/scala/services/ReportMailer.scala
@@ -5,20 +5,23 @@ import java.io.File
 import courier.{Envelope, Mailer, Multipart}
 import interfaces.{ErrorPayload, SuccessResult}
 import javax.mail.internet.InternetAddress
+import monix.eval.Task
 
 trait ReportMailer {
 
-  def sendReport(sender: String, subject: String, report: File, body: String)(implicit mailer: Mailer): Either[ErrorPayload, SuccessResult] = {
+  def sendReport(sender: String, subject: String, report: File, body: String)(implicit mailer: Mailer): Task[Unit] = {
 
     val address: InternetAddress = new InternetAddress(sender)
-    Either.cond(template.isSuccess, template.get, ErrorPayload(404, s"Cannot find template $templateName"))
+    var attempts: Int = 0
 
-    val mailAction = mailer(Envelope.from(address)
-          .to(address)
-          .subject(subject)
-          .content(Multipart()
-            .attach(report)
-            .html(body))).onComplete(_ => Right9(SuccessResult("Report Delivered"))
+    Task.eval{
+      mailer(Envelope.from(address)
+        .to(address)
+        .subject(subject)
+        .content(Multipart()
+          .attach(report)
+          .html(body))).onComplete(_ => {attempts + 1; println("Report Delivered")})
+    }.onErrorRestartIf(_ => attempts < 3)
 
   }
 }
